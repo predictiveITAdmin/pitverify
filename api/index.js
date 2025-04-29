af21896aca0e8135f34e7ab0d6fd3cb79aeb85d6
bhavesh132 <bhavesh25515@gmail.com>
2025-04-29 17:13:49 -0400

    rewrite backend in commonJS

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const createHandler = require('azure-function-express').createHandler;
+const createHandler = require('azure-function-express').createHandler
 const app = require('./server');
 
 module.exports = createHandler(app);
