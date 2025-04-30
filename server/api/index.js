dd90d99596dfc81024da13870e43ecc19ea5cbc1
bhavesh132 <bhavesh25515@gmail.com>
2025-04-30 18:11:01 -0400

    logging for error details

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,4 @@
-const createHandler = require('azure-function-express').createHandler
+const { createAzureFunctionHandler } = require('azure-function-express');
 const app = require('./server');
 
-module.exports = createHandler(app);
+module.exports = createAzureFunctionHandler(app);
