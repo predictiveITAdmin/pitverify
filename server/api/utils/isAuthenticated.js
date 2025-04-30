e9f61e19d53b58e169f841e331fc16b174cc5efa
bhavesh132 <bhavesh25515@gmail.com>
2025-04-30 18:01:20 -0400

    logging for error details

--- server/api/utils/isAuthenticated.js
+++ server/api/utils/isAuthenticated.js
@@ -1,5 +1,6 @@
-
-export default function isAuthenticated(req, res, next) {
+function isAuthenticated(req, res, next) {
   if (req.session?.user) return next();
   res.status(401).json({ error: 'Unauthorized' });
 }
+
+module.exports = isAuthenticated;
