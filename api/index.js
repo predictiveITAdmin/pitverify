55c4781369b52dd1423d9c1299e1069d779a57a6
bhavesh132 <bhavesh25515@gmail.com>
2025-04-30 14:48:16 -0400

    Re-wired the Config Structure

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const createHandler = require('azure-function-express').createHandler
-const app = require('./server');
-
-module.exports = createHandler(app);
--- api/utils/isAuthenticated.js
+++
@@ -1,5 +0,0 @@
-
-export default function isAuthenticated(req, res, next) {
-  if (req.session?.user) return next();
-  res.status(401).json({ error: 'Unauthorized' });
-}
