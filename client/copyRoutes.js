84f2ed3d840a10343b9d0d3a9c7e486c1ede4436
bhavesh132 <bhavesh25515@gmail.com>
2025-05-01 14:57:38 -0400

    updated workflow

--- client/copyRoutes.js
+++ client/copyRoutes.js
@@ -1,6 +1,5 @@
-// copyRoutes.js
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';    
 
 const src = path.join(__dirname, 'client', 'routes.json');
 const dest = path.join(__dirname, 'client', 'dist', 'routes.json');
